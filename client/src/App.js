8fc4210333c2aad5367b1976a02aae9c9ca64985
wykwark <wykwark@gmail.com>
2022-11-28 12:16:22 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -22,41 +22,58 @@ const styles = theme =>({
 
 )
 
-const customers =[
-{
-  'id':1,
-  'image':'https://placeimg.com/64/64/3',
-'name' : '김치국',
-'birthday':'96122',
-'gender':'남자',
-'job':'대학생'
+// const customers =[
+// {
+//   'id':1,
+//   'image':'https://placeimg.com/64/64/3',
+// 'name' : '김치국',
+// 'birthday':'96122',
+// 'gender':'남자',
+// 'job':'대학생'
 
-},
-{
-  'id':2,
-  'image':'https://placeimg.com/64/64/5',
-'name' : '홍길동',
-'birthday':'96122',
-'gender':'남자',
-'job':'대학생'
+// },
+// {
+//   'id':2,
+//   'image':'https://placeimg.com/64/64/5',
+// 'name' : '홍길동',
+// 'birthday':'96122',
+// 'gender':'남자',
+// 'job':'대학생'
 
-},
-{
-  'id':3,
-  'image':'https://placeimg.com/64/64/2',
-'name' : '나동빈',
-'birthday':'96122',
-'gender':'남자',
-'job':'대학생'
+// },
+// {
+//   'id':3,
+//   'image':'https://placeimg.com/64/64/2',
+// 'name' : '나동빈',
+// 'birthday':'96122',
+// 'gender':'남자',
+// 'job':'대학생'
 
-}
+// }
  
 
-]
+// ]
 
 class App extends Component {
 
- render(){
+  state={
+    customers:""
+
+  }
+componentDidMount(){
+  this.callApi()
+  .then(res => this.setState({customers:res}))
+  .catch(err => console.log(err));
+}
+callApi = async() => {
+const response = await fetch('/api/customers');
+const body = await response.json();
+return body;
+}
+
+ 
+ 
+  render(){
 
   const {classes} = this.props
   return (
@@ -73,8 +90,7 @@ class App extends Component {
         </TableRow>
       </TableHead>
       <TableBody>
-      {
-      customers.map(c =>{
+      { this.state.customers ? this.state.customers.map(c =>{
         return(<
           Customer
           key={c.id}
@@ -86,10 +102,7 @@ class App extends Component {
           job={c.job}
          />
         );
-
-      })
-
-    } 
+      }):""} 
       </TableBody>
     </Table>
 

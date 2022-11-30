5f30d84b9ad690584cee0b92b0e50e873335008e
wykwark <wykwark@gmail.com>
2022-11-30 10:07:01 +0900

    로딩하는 것 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,38 @@
-import React,{Component} from 'react';
+
+import React,{Component} from "react";
 import Customer from './components/Customer';
 import './App.css';
 import Table from '@material-ui/core/Table';
-// import paper from '@material-ui/core/Paper';
+ import paper from '@material-ui/core/Paper';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 // import {withstyles} from '@material-ui/core/styles';
+import {CircularProgress} from "@material-ui/core";
+
+const styles = theme => ({
+  root:{
+    width:'100',
+    marginTop:theme.spacing.unit *3,
+    overflowX: "auto"
+  },
+  table:{
+    minWidth:1080
+  },
+  progress:{
+    margin:theme.spacing.unit*2
+  }
+});
 class App extends Component {
 
   state={
-   customers:''
+   customers:"",
+   completed:0
 
   }
 componentDidMount(){
+  this.timer = setInterval(this.progress,20)  ;
   this.callApi()
   .then(res => this.setState({customers:res}))
   .catch(err => console.log(err));
@@ -23,6 +41,11 @@ callApi = async() => {
 const response = await fetch('/api/customers');
 const body = await response.json();
 return body;
+}
+
+progress =() => {
+    const {completed} = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed + 1});
 }
 
  
@@ -56,7 +79,13 @@ return body;
           job={c.job}
          />
         );
-      }):''} 
+      }):
+        <TableRow>
+          <TableCell colSpan="6" align ="center">
+            <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+          </TableCell>
+        </TableRow>
+      }
       </TableBody>
     </Table>
 
